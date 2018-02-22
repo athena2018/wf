1c712f8d6a58d323078f7b44a50f1fa14500f9a6
athena2018 <lesgayard@gmail.com>
2018-02-22 09:38:52 +0100

    envoi deux

--- SQL/Exercice2/exercisetwo.sql
+++ SQL/Exercice2/exercisetwo.sql
@@ -1,2 +1,5 @@
 
-SELECT agent_code, SUM(amount), count(id) FROM test.orders WHERE amount >=1000 GROUP BY agent_code ORDER BY SUM(amount) DESC;
+SELECT agent_code, SUM(amount), COUNT(id) FROM test.orders WHERE amount >=1000 GROUP BY agent_code ORDER BY AVG(amount) DESC;
+SELECT agent_code, SUM(amount), COUNT (id) FROM orders WHERE amount >=1000 GROUP BY agent_code;
+SELECT agent_code, SUM (amount), COUNT (id)FROM orders WHERE amount >=1000 GROUP BY agent_code ORDER BY AVG(amount);
+SELECT agent_code, SUM (amount), COUNT(id) FROM orders WHERE amount >=1000 GROUP BY agen_code ORDER BY AVG (amount) DESC;agent_code
